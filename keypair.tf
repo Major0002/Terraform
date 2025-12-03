c154a84f7f093c410132eb742556217893900211
Major0002 <davidoluwaseun743@gmail.com>
2025-12-03 09:55:21 +0100

    Stop tracking keypair directory

--- keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "major-key" {
-  key_name   = "major-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPPLADF/CBvGJHNepjIs+JSb72UhP5D4eEX6jR6ocItZ pc@DESKTOP-ASO79GK"
-}
